a65b4dc809030eaaafdc173ee18991be2f603069
SydUNC <sydneyg@live.unc.edu>
2022-08-28 14:57:13 -0400

    Server no longer times out and successfully takes port argument, error handling still to be tweaked

--- server.js
+++ server.js
@@ -8,20 +8,13 @@ const args = require('minimist')(process.argv.slice(2));
 
 const port = (args.port || 3000);
 
-fs.readFile('./public/index.html', (err, data) => {
-	   if (err) {
-		         console.error(err);
-		         return;
-		       }
-	    console.log(data);
-});
-
 const server = http.createServer((req, res) => {
 	   res.statusCode = 200
 	   res.setHeader('Content-Type', 'text/html')
-	   res.end(data)
+	   res.end(`data`)
 });
 
 server.listen(port, () => {
-	   console.log('Server listening on port ${port}')
+	   console.log(`Server listening on port ${port}`)
 });
+

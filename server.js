2417745926d1b822e31cafb71da31cf1f3e6041d
SydUNC <sydneyg@live.unc.edu>
2022-08-28 15:05:49 -0400

    Server error handling successful.

--- server.js
+++ server.js
@@ -8,13 +8,20 @@ const args = require('minimist')(process.argv.slice(2));
 
 const port = (args.port || 3000);
 
-const server = http.createServer((req, res) => {
-	   res.statusCode = 200
-	   res.setHeader('Content-Type', 'text/html')
-	   res.end(`data`)
-});
+fs.readFile('./public/index.html', (err, data) => {
+	   if (err) {
+		         console.error(err);
+		         return;
+		       }
+	    const server = http.createServer((req, res) => {
+		       	res.statusCode = 200
+		       	res.setHeader('Content-Type', 'text/html')
+		       	res.end(data)
+		    	});
 
-server.listen(port, () => {
-	   console.log(`Server listening on port ${port}`)
+	    server.listen(port, () => {
+		       		console.log(`Server listening on port ${port}`)
+		    	});
+
 });
 
